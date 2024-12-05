7f9bac19d04639464c9837e82d8165c6813a5a69
CharEasy <489644865@qq.com>
2024-12-06 00:19:08 +0800

    import element ui

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,24 +1,26 @@
 import '../css/app.css';
 import './bootstrap';
 
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { createApp, h } from 'vue';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import {createInertiaApp} from '@inertiajs/vue3';
+import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers';
+import {createApp, h} from 'vue';
+import {ZiggyVue} from '../../vendor/tightenco/ziggy';
+
+
+// 引入 Element UI
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) =>
-        resolvePageComponent(
-            `./Pages/${name}.vue`,
-            import.meta.glob('./Pages/**/*.vue'),
-        ),
-    setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue'),),
+    setup({el, App, props, plugin}) {
+        return createApp({render: () => h(App, props)})
             .use(plugin)
             .use(ZiggyVue)
+            .use(ElementPlus) // 使用 Element UI
             .mount(el);
     },
     progress: {
